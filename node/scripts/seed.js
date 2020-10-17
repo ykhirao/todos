1ce894167d5258fff76aa735ba6cac6c741c9e50
Yuki <yuki.dees39th@gmail.com>
2020-10-17 22:14:33 +0900

    fix seed

--- node/scripts/seed.js
+++ node/scripts/seed.js
@@ -6,19 +6,24 @@ const db = new sqlite3.Database('data.sqlite3')
 db.serialize(function() {
   console.log("SEED is run!!!!!!!")
   db.run('DROP TABLE IF EXISTS todos;');
-  db.run('CREATE TABLE todos(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, completed INTEGER);');
+  db.run(`CREATE TABLE todos(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      text TEXT,
+      completed INTEGER,
+      created_at TEXT DEFAULT (DATETIME(CURRENT_TIMESTAMP, 'localtime'))
+    );`);
 
-    var stmt = db.prepare('INSERT INTO todos (text, completed) VALUES (?, 0)')
+    var stmt = db.prepare('INSERT INTO todos (text, completed) VALUES (?, ?)')
 
     const todos = [
-      "洗濯物（サンプル）",
-      "食器洗い（サンプル）",
-      "プログラミング（サンプル）",
+      { text: "洗濯物（サンプル）", completed: 0 },
+      { text: "食器洗い（サンプル）", completed: 1 },
+      { text: "プログラミング（サンプル）", completed: 0 },
     ];
 
     todos.forEach(todo => {
-      stmt.run(todo)
+      stmt.run(todo.text, todo.completed)
     });
-
+  
     stmt.finalize()
 })
